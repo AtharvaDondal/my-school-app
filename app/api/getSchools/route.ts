f0559cb80431152fddc404a3cff09c00d8cd056b
AtharvaDondal <dondalatharva@gmail.com>
2025-09-02 09:43:57 +0530

    fix some changes

--- app/api/getSchools/route.ts
+++ app/api/getSchools/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextRequest, NextResponse } from "next/server";
 import pool from "../../lib/db";
 
@@ -8,6 +9,8 @@ loadEnvConfig(projectDir);
 export async function GET(req: NextRequest) {
   try {
     const [rows] = await pool.query("SELECT * FROM schools ORDER BY id DESC");
+    
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const schools = (rows as any[]).map((row) => ({
       ...row,
       imageUrl: row.imageUrl, // use imageUrl directly
